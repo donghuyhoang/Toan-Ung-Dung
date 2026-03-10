6d9b4c9085631a46f2a9299502b9986d55596e97
Dong Huy Hoang <70799199+donghuyhoang@users.noreply.github.com>
2026-03-10 13:43:56 +0700

    Update Bai2.cpp

--- Toan-Ung-Dung/Tuan3/102240366-Đồng Huy Hoàng/Bai2.cpp
+++ Toan-Ung-Dung/Tuan3/102240366-Đồng Huy Hoàng/Bai2.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
-#include <math.h>
-#include <vector>
+#include <cmath>
+#include <climits>
+
 using namespace std;
 
 bool is_prime(int n){
-    for (int i = 2; i <= sqrt(n); i++){
+    if(n < 2) return false;
+    for(int i = 2; i * i <= n; i++){
         if(n % i == 0) return false;
     }
-    return n > 1;
+    return true;
 }
+
 int main(){
     int n,m;
-    cout << "So luong cac so nguyen to nam trong n m (khong tinh n m)" << endl;
-    cout << "Nhap n m: " << endl; cin >> n >> m;
-    int mi = 1000,res = 0,cnt = 0;
-    for (int i = n + 1; i < m; i++){
-        if(is_prime(i)) {
-            ++cnt;
-            if(abs(i - 636) <= mi) {
+
+    cout << "Nhap n m: ";
+    cin >> n >> m;
+
+    if(n >= m){
+        cout << "Khoang khong hop le";
+        return 0;
+    }
+
+    int mi = INT_MAX;
+    int res = 0;
+    int cnt = 0;
+
+    for(int i = n + 1; i < m; i++){
+        if(is_prime(i)){
+            cnt++;
+
+            if(abs(i - 636) < mi){
                 res = i;
-                mi = abs(i-636);
+                mi = abs(i - 636);
             }
         }
     }
-    cout << "So luong so nguyen to trong khoang n m: " << cnt << endl;
-    cout << "So nguyen to gan 636 nhat: " << res;
+
+    cout << "So luong so nguyen to: " << cnt << endl;
+
+    if(cnt > 0)
+        cout << "So nguyen to gan 636 nhat: " << res;
+    else
+        cout << "Khong co so nguyen to trong khoang";
 }
